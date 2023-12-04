330377a527e1bc00eb09226c90d7774607e6dee1
unknown <yjm020811>
2023-12-04 22:01:23 +0800

    关键词搜索和分类销售开发完毕

--- 04_数据可视化报表项目/datascreen/src/components/BottomView/index.vue
+++ 04_数据可视化报表项目/datascreen/src/components/BottomView/index.vue
@@ -8,30 +8,33 @@
             <div class="title">关键词搜索</div>
           </div>
         </template>
-        <!-- body的chart部分 -->
         <div class="chart-wrapper">
-          <div class="chart">
-            <div class="chart-title">搜索用户数</div>
-            <div class="chart-data">93,643</div>
-            <div id="chart-search" style="width: 20%; height: 100%"></div>
+          <!-- body的chart部分 -->
+          <div class="chart-inner">
+            <div class="chart">
+              <div class="chart-title">搜索用户数</div>
+              <div class="chart-data">93,643</div>
+              <div id="chart-search" style="width: 380px; height: 130px"></div>
+            </div>
+            <div class="chart">
+              <div class="chart-title">搜索量</div>
+              <div class="chart-data">198,643</div>
+              <div id="chart-searchNum" style="width: 380px; height: 130px"></div>
+            </div>
           </div>
-          <div class="chart">
-            <div class="chart-title">搜索量</div>
-            <div class="chart-data">198,643</div>
-            <div id="chart-searchNum" style="width: 20%; height: 100%"></div>
+          <!-- body的table部分 -->
+          <div class="table-wrapper">
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="rank" label="排名" width="180">
+              </el-table-column>
+              <el-table-column prop="address" label="地址" width="180">
+              </el-table-column>
+              <el-table-column prop="users" label="用户"> </el-table-column>
+              <el-table-column prop="range" label="点击率"> </el-table-column>
+            </el-table>
+            <el-pagination layout="prev, pager, next" :total="100" :page-size="4" background>
+            </el-pagination>
           </div>
-        </div>
-        <!-- body的table部分 -->
-        <div class="table-wrapper">
-          <el-table :data="tableData" style="width: 100%">
-            <el-table-column prop="date" label="日期" width="180">
-            </el-table-column>
-            <el-table-column prop="name" label="姓名" width="180">
-            </el-table-column>
-            <el-table-column prop="address" label="地址"> </el-table-column>
-          </el-table>
-          <el-pagination layout="prev, pager, next" :total="50">
-          </el-pagination>
         </div>
       </el-card>
     </div>
@@ -48,7 +51,9 @@
             </div>
           </div>
         </template>
-        <div></div>
+        <div class="chart-wrapper">
+          <div id="peiChart" style="width: 700px; height: 400px;"></div>
+        </div>
       </el-card>
     </div>
   </div>
@@ -62,28 +67,41 @@ export default {
     return {
       chartInstance1: "",
       chartInstance2: "",
+      chartInstance3: "",
       radioSelect: "品类",
       tableData: [
         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
+          id: "1",
+          rank: "1",
+          address: "北京",
+          count: 100,
+          users: 90,
+          range: "90%"
         },
         {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
+          id: "2",
+          rank: "2",
+          address: "达州",
+          count: 100,
+          users: 90,
+          range: "90%"
         },
         {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
+          id: "3",
+          rank: "3",
+          address: "洛杉矶",
+          count: 100,
+          users: 90,
+          range: "90%"
         },
         {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
+          id: "4",
+          rank: "4",
+          address: "成都",
+          count: 100,
+          users: 90,
+          range: "90%"
+        },
       ]
     };
   },
@@ -93,50 +111,27 @@ export default {
     this.chartInstance1 = chart1;
     window.addEventListener("resize", this.handleResize);
     chart1.setOption({
-      title: {
-        text: "年度销售额",
-        top: 0,
-        left: "2.5%",
-        textStyle: {
-          fontSize: 14
-        }
-      },
       xAxis: {
         type: "category",
-        data: [
-          "1月",
-          "2月",
-          "3月",
-          "4月",
-          "5月",
-          "6月",
-          "7月",
-          "8月",
-          "9月",
-          "10月",
-          "11月",
-          "12月"
-        ],
-        axisLine: {
-          // x 轴线设置
-          show: true // 显示 x 轴线
-        }
+        boundaryGap: false
       },
       yAxis: {
-        type: "value",
-        axisLine: {
-          // y 轴线设置
-          show: true // 显示 y 轴线
-        }
+        show: false,
       },
       series: [
         {
           data: [120, 200, 150, 80, 70, 110, 130, 100, 210, 100, 200, 300],
-          type: "bar",
-          barWidth: "25%",
+          type: "line",
+          areaStyle: {
+            color: "rgba(95,187,255,0.5)"
+          },
           itemStyle: {
-            color: "#3291cf"
-          }
+            opacity: 0
+          },
+          lineStyle: {
+            color: "rgba(95,187,255)"
+          },
+          smooth: true
         }
       ],
       grid: {
@@ -144,62 +139,34 @@ export default {
         left: "3%",
         right: "4%",
         bottom: "3%",
-        containLabel: true // 是否包含坐标轴的刻度标签
       },
-      tooltip: {
-        show: true,
-        trigger: "axis"
-      }
     });
     const chartDom2 = document.getElementById("chart-searchNum");
     const chart2 = this.$echarts.init(chartDom2);
     this.chartInstance2 = chart2;
     window.addEventListener("resize", this.handleResize);
     chart2.setOption({
-      title: {
-        text: "年度销售额",
-        top: 0,
-        left: "2.5%",
-        textStyle: {
-          fontSize: 14
-        }
-      },
       xAxis: {
         type: "category",
-        data: [
-          "1月",
-          "2月",
-          "3月",
-          "4月",
-          "5月",
-          "6月",
-          "7月",
-          "8月",
-          "9月",
-          "10月",
-          "11月",
-          "12月"
-        ],
-        axisLine: {
-          // x 轴线设置
-          show: true // 显示 x 轴线
-        }
+        boundaryGap: false
       },
       yAxis: {
-        type: "value",
-        axisLine: {
-          // y 轴线设置
-          show: true // 显示 y 轴线
-        }
+        show: false,
       },
       series: [
         {
           data: [120, 200, 150, 80, 70, 110, 130, 100, 210, 100, 200, 300],
-          type: "bar",
-          barWidth: "25%",
+          type: "line",
+          areaStyle: {
+            color: "rgba(95,187,255,0.5)"
+          },
           itemStyle: {
-            color: "#3291cf"
-          }
+            opacity: 0
+          },
+          lineStyle: {
+            color: "rgba(95,187,255)"
+          },
+          smooth: true
         }
       ],
       grid: {
@@ -207,14 +174,119 @@ export default {
         left: "3%",
         right: "4%",
         bottom: "3%",
-        containLabel: true // 是否包含坐标轴的刻度标签
+      },
+    });
+    const chartDom3 = document.getElementById("peiChart");
+    const chart3 = this.$echarts.init(chartDom3);
+    this.chartInstance3 = chart3;
+    window.addEventListener("resize", this.handleResize);
+    chart3.setOption({
+      title: [{
+        text: "品类分布",
+        textStyle: {
+          fontSize: 14,
+          color: "#666"
+        },
+        left: 20,
+        top: 20,
+      },
+      {
+        text: "累计订单量",
+        subtext: "320",
+        x: '34.5%',
+        y: '42.5%',
+        textAlign: "center",
+        textStyle: {
+          fontSize: 14,
+          color: "#999"
+        },
+        subtextStyle: {
+          fontSize: 28,
+          color: "#333"
+        }
+      }
+      ],
+      series: [{
+        type: "pie",
+        name: "品类分布",
+        data: [
+          {
+            legendName: "粉面粥店",
+            value: 67,
+            percentage: '15.4%',
+            itemStyle: {
+              color: 'red'
+            },
+            name: "粉面粥店|15.4%"
+          },
+          {
+            legendName: "简餐便当",
+            value: 97,
+            percentage: '22.30%',
+            name: '简餐便当|22.30%'
+          }, {
+            legendName: "汉堡披萨",
+            value: 92,
+            percentage: '21.15%',
+            name: '汉堡披萨|21.15%'
+          },
+          {
+            legendName: "螺蛳粉",
+            value: 67,
+            percentage: '15.4%',
+            name: '螺蛳粉|15.4%'
+          }, {
+            legendName: "海底捞",
+            value: 67,
+            percentage: '15.4%',
+            name: '海底捞|15.4%'
+          }
+        ],
+        label: {
+          normal: {
+            show: true,
+            position: "outter",
+            formatter: function (params) {
+              return params.data.legendName
+            }
+          }
+        },
+        center: ['35%', '50%'],
+        /* 内半径与外半径 */
+        radius: ['45%', '60%'],
+        labelLine: {
+          normal: {
+            length: 5,
+            length2: 3,
+            smooth: true
+          }
+        },
+        clockwise: false,
+        itemStyle: {
+          borderWidth: 6,
+          borderColor: '#fff'
+        }
+      }],
+      legend: {
+        type: "scroll",
+        orient: 'vertical',
+        left: '70%',
+        top: "middle",
+        textStyle: {
+          color: '#8c8c8c'
+        }
       },
       tooltip: {
-        show: true,
-        trigger: "axis"
+        trigger: 'item',
+        formatter: function (params) {
+          return params.seriesName + '<br />' +
+            params.marker + params.data.legendName + '<br />' +
+            '数量:' + params.data.value + '<br />' +
+            '占比:' + params.data.percentage
+        }
       }
     });
-  }
+  },
 };
 </script>
 <style scoped>
@@ -248,14 +320,38 @@ export default {
 }
 
 .chart-wrapper {
-  .chart {
-    .chart-title {
-      color: gray;
+  display: flex;
+  flex-direction: column;
+  height: 452px;
+
+  .chart-inner {
+    display: flex;
+    padding: 0 10px;
+
+    .chart {
+      flex: 1;
+      padding: 0 10px;
+
+      .chart-title {
+        color: #999;
+        font-size: 14px
+      }
+
+      .chart-data {
+        font-size: 22px;
+        color: #333;
+        font-weight: 600;
+        letter-spacing: 2px;
+      }
     }
-    .chart-data {
-      font-weight: 600;
-      font-size: 22px;
-    }
+  }
+}
+
+.table-wrapper {
+  .el-pagination {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 10px;
   }
 }
 
